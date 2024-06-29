29e0091740702d8e33ae219a97a850fb5580552b
sw1227 <32032108+sw1227@users.noreply.github.com>
2024-06-29 19:05:08 +0900

    feat: add svg with adaptive size

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState, useEffect, useRef } from 'react'
 import { useRecoilState, useRecoilValue } from 'recoil'
 import './App.css'
 import { Box, Heading, VStack } from '@chakra-ui/react'
@@ -21,6 +21,27 @@ const App: FC = () => {
   const pitchDiameter = useRecoilValue(pitchDiameterState)
   const pressureAngle = useRecoilValue(pressureAngleState)
 
+  // svg size
+  const [svgSize, setSvgSize] = useState<number>(0);
+  const svgContainerRef = useRef<HTMLDivElement>(null);
+
+  // Max size of svg is 800
+  const updateSize = () => {
+    if (svgContainerRef.current) {
+      const newSize = Math.min(svgContainerRef.current.clientWidth, 800);
+      setSvgSize(newSize);
+    }
+  };
+
+  // Update size when mounted and resized
+  useEffect(() => {
+    updateSize();
+    window.addEventListener('resize', updateSize);
+    return () => {
+      window.removeEventListener('resize', updateSize);
+    };
+  }, []);
+
   return (
     <>
       <Box p={4}>
@@ -35,6 +56,13 @@ const App: FC = () => {
         </VStack>
 
         <Box mt={8}>teethNumber = {teethNumber}, module = {module}, pitch diameter={pitchDiameter}, pressure angle={pressureAngleDegree}[deg] ({pressureAngle.toFixed(3)}[rad])</Box>
+
+        <Box mt={8} ref={svgContainerRef} display="flex" justifyContent="center" boxSizing='border-box'>
+          {/* TODO: 歯車の収まるviewBoxを設定 */}
+          <svg xmlns="http://www.w3.org/2000/svg" width={svgSize} height={svgSize} viewBox="-50 -50 100 100">
+            <circle cx="0" cy="0" r="50" fill="none" stroke="blue" />
+          </svg>
+        </Box>
       </Box>
     </>
   )
